40c6ac37d57077d34cf7fcaa6a3acfaed839e8fb
cartogram <matt.seccafien@shopify.com>
2019-04-01 14:38:00 -0400

    Adds useImportRemote hook

--- packages/react-import-remote/src/ImportRemote.tsx
+++ packages/react-import-remote/src/ImportRemote.tsx
@@ -1,15 +1,11 @@
 import * as React from 'react';
 import {Preconnect} from '@shopify/react-html';
-import {
-  RequestIdleCallbackHandle,
-  WindowWithRequestIdleCallback,
-  DeferTiming,
-} from '@shopify/async';
+import {DeferTiming} from '@shopify/async';
 import {
   IntersectionObserver,
   UnsupportedBehavior,
 } from '@shopify/react-intersection-observer';
-import load from './load';
+import {useImportRemote} from './hooks';
 
 export interface Props<Imported = any> {
   source: string;
@@ -21,91 +17,58 @@ export interface Props<Imported = any> {
   defer?: DeferTiming;
 }
 
-interface State {
-  loaded: boolean;
-  loading: boolean;
-}
+export function ImportRemote(props: Props) {
+  const {
+    source,
+    preconnect,
+    nonce,
+    defer,
+    getImport,
+    onError,
+    onImported,
+  } = props;
 
-export default class ImportRemote extends React.PureComponent<Props, State> {
-  state: State = {loaded: false, loading: false};
-  private idleCallbackHandle?: RequestIdleCallbackHandle;
+  const {loading, loaded, error, imported} = useImportRemote(
+    source,
+    {defer, nonce},
+    getImport,
+  );
 
-  componentWillUnmount() {
-    if (this.idleCallbackHandle != null && 'cancelIdleCallback' in window) {
-      (window as WindowWithRequestIdleCallback).cancelIdleCallback(
-        this.idleCallbackHandle,
-      );
-    }
-  }
-
-  async componentDidMount() {
-    const {defer = DeferTiming.Mount} = this.props;
-
-    if (defer === DeferTiming.Idle) {
-      if ('requestIdleCallback' in window) {
-        this.idleCallbackHandle = (window as WindowWithRequestIdleCallback).requestIdleCallback(
-          this.loadRemote,
-        );
-      } else {
-        this.loadRemote();
+  React.useEffect(
+    () => {
+      if (error != null) {
+        onError(error);
       }
-    } else if (defer === DeferTiming.Mount) {
-      await this.loadRemote();
-    }
+    },
+    [error, onError],
+  );
+
+  React.useEffect(
+    () => {
+      onImported(imported);
+    },
+    [imported, onImported],
+  );
+
+
+  const intersectionObserver =
+    !loaded && !loading && defer === DeferTiming.InViewport ? (
+      <IntersectionObserver
+        threshold={0}
+        unsupportedBehavior={UnsupportedBehavior.TreatAsIntersecting}
+        onIntersecting={this.loadRemote}
+      />
+    ) : null;
+
+  if (preconnect) {
+    const url = new URL(source);
+    return (
+      <>
+        <Preconnect source={url.origin} />
+        {intersectionObserver}
+      </>
+    );
   }
 
-  async componentDidUpdate({source: oldSource}: Props) {
-    const {source} = this.props;
-
-    if (oldSource !== source) {
-      await this.loadRemote();
-    }
-  }
-
-  render() {
-    const {loaded, loading} = this.state;
-    const {source, preconnect, defer} = this.props;
-
-    const intersectionObserver =
-      !loaded && !loading && defer === DeferTiming.InViewport ? (
-        <IntersectionObserver
-          threshold={0}
-          unsupportedBehavior={UnsupportedBehavior.TreatAsIntersecting}
-          onIntersectionChange={this.loadRemoteIfIntersecting}
-        />
-      ) : null;
-
-    if (preconnect) {
-      const url = new URL(source);
-      return (
-        <>
-          <Preconnect source={url.origin} />
-          {intersectionObserver}
-        </>
-      );
-    }
-
-    return intersectionObserver;
-  }
-
-  private loadRemoteIfIntersecting = ({isIntersecting = true}) => {
-    return isIntersecting ? this.loadRemote() : Promise.resolve();
-  };
-
-  private loadRemote = () => {
-    return new Promise(resolve => {
-      this.setState({loaded: false, loading: true}, async () => {
-        const {source, nonce = '', getImport, onError, onImported} = this.props;
-
-        try {
-          const imported = await load(source, getImport, nonce);
-          onImported(imported);
-        } catch (error) {
-          onError(error);
-        } finally {
-          this.setState({loaded: true, loading: false}, resolve);
-        }
-      });
-    });
-  };
+  return intersectionObserver;
 }
